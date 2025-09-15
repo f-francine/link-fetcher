e5ce8aba492b25fddf4361c74c2ec3408a7ae476
Francine Guimarães <66988170+f-francine@users.noreply.github.com>
2025-09-15 13:01:16 -0300

    feat: paginate links (#4)

--- lib/link_fetcher_web/live/pages_live/show.ex
+++ lib/link_fetcher_web/live/pages_live/show.ex
@@ -2,7 +2,8 @@ defmodule LinkFetcherWeb.PagesLive.Show do
   use LinkFetcherWeb, :live_view
 
   alias LinkFetcher.Pages
-  alias LinkFetcher.Pages.Page
+
+  import LinkFetcherWeb.Pagination
 
   require Logger
 
@@ -11,13 +12,16 @@ defmodule LinkFetcherWeb.PagesLive.Show do
     user = LinkFetcher.Accounts.get_user(session["current_user_id"])
 
     if authenticated?(user) do
-      page = LinkFetcher.Pages.get_page(params["page_id"])
+      page_number = 1
+      {paginated_links, total_pages} = Pages.paginated_links(params["page_id"], page_number)
 
       {:ok,
        socket
        |> assign(:page_title, "Details")
        |> assign(:current_user_id, user.id)
-       |> assign(:page, page)}
+       |> assign(:links, paginated_links)
+       |> assign(:total_pages, total_pages)
+       |> assign(:page_number, page_number)}
     else
       {:ok,
        socket
@@ -26,11 +30,23 @@ defmodule LinkFetcherWeb.PagesLive.Show do
     end
   end
 
-  @impl true
-  def handle_event("show", %{"page_id" => id}, socket) do
-    page = Pages.get_page(id)
+  # @impl true
+  # def handle_event("show", %{"page_id" => id}, socket) do
+  #   page = Pages.get_page(id)
 
-    {:noreply, socket |> assign(page: page)}
+  #   {:noreply, socket |> assign(links: page)}
+  # end
+
+  @impl true
+  def handle_event("paginate", %{"page" => page}, socket) do
+    page_number = String.to_integer(page)
+    {paginated_links, total_pages} = Pages.paginated_links(socket.assigns.page.id, page_number)
+
+    {:noreply,
+     socket
+     |> assign(links: paginated_links)
+     |> assign(:total_pages, total_pages)
+     |> assign(:page_number, page_number)}
   end
 
   @impl true
@@ -38,10 +54,9 @@ defmodule LinkFetcherWeb.PagesLive.Show do
     page = Pages.get_page(params["page_id"])
 
     {:noreply,
-      socket
-      |> assign(:page_title, "Details")
-      |> assign(:page, page)
-    }
+     socket
+     |> assign(:page_title, "Details")
+     |> assign(:page, page)}
   end
 
   defp authenticated?(user), do: if(user == nil, do: false, else: true)
